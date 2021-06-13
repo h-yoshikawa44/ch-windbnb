aade43695cd516168c15631bf792689c8a063cf3
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-06-13 13:16:17 +0900

    settings: .editorconfig、ESLint、Prettier、StyleLintセットアップ(#1)
    StyleLintはCSS in JS 形式の設定。

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,6 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' });
 };
